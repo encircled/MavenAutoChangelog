65065268c2d93f837c817b38bfa60a2fc98ea342
Vlad Kisel <eencircled@gmail.com>
2017-07-17 20:30:37 +0200

    Duplicate first row fix

--- src/main/java/cz/encircled/macl/GitLogParser.java
+++ src/main/java/cz/encircled/macl/GitLogParser.java
@@ -1,13 +1,14 @@
 package cz.encircled.macl;
 
+import org.apache.maven.plugin.logging.Log;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NavigableSet;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
-
-import org.apache.maven.plugin.logging.Log;
 
 /**
  * @author Kisel on 22.6.2017.
@@ -27,7 +28,8 @@ public class GitLogParser implements VCSLogParser {
         String command = String.format(GitLogParser.command, tagFrom);
         Process p = Runtime.getRuntime().exec(command);
         BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        List<String> result = input.lines()
+
+        TreeSet<String> result = input.lines()
                 .map(String::trim)
                 .filter(l -> {
                     boolean matches = conf.applicableCommitPattern.matcher(l).matches();
@@ -37,15 +39,16 @@ public class GitLogParser implements VCSLogParser {
                     return matches;
                 })
                 .map(s -> String.format(conf.commitFormat, s))
-                .collect(Collectors.toList());
+                .collect(Collectors.toCollection(TreeSet::new));
 
         if (!result.isEmpty()) {
-            result.set(0, "\r\n" + result.get(0));
+            List<String> temp = new ArrayList<>(result);
+            temp.set(0, "\r\n" + temp.get(0));
+            result = new TreeSet<>(temp);
         }
-
         log.info(String.format("Executed command [%s] returned [%d] rows", command, result.size()));
 
-        return new TreeSet<>(result);
+        return result;
     }
 
 }
