cef6284d1ce423c169ce2f183b20a74bfcaa9bf6
vladislav kisel <vladislav.kisel@dhl.com>
2022-07-22 11:33:35 +0200

    Add unit test

--- src/test/java/cz/encircled/macl/ConfigurationTest.java
+++ src/test/java/cz/encircled/macl/ConfigurationTest.java
@@ -1,7 +1,11 @@
 package cz.encircled.macl;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * @author Vlad on 17-Nov-17.
@@ -20,8 +24,15 @@ public class ConfigurationTest extends AbstractTest {
     @Test
     public void testEmptyPatterns() {
         ChangelogConfiguration conf = base().setMergeRequestReplacePattern(null).setLastTagPattern("");
-        Assert.assertNull(conf.mergeRequestReplacePattern);
-        Assert.assertNull(conf.lastTagPattern);
+        assertNull(conf.mergeRequestReplacePattern);
+        assertNull(conf.lastTagPattern);
+    }
+
+    @Test
+    public void testNonEmptyPatterns() {
+        ChangelogConfiguration conf = base().setMergeRequestReplacePattern(".*").setLastTagPattern(".*");
+        assertEquals(Pattern.compile(".*").pattern(), conf.mergeRequestReplacePattern.pattern());
+        assertEquals(Pattern.compile(".*").pattern(), conf.lastTagPattern.pattern());
     }
 
     private ChangelogConfiguration base() {
